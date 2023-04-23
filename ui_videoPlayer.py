4e099267a63a1ed09d356cb19e4a18a1d5671175
superttogi <111804023+superttogi@users.noreply.github.com>
2023-04-23 10:53:06 -0700

    Update ui_videoPlayer.py
    Insert Video frames into initial form.

--- ui_videoPlayer.py
+++ ui_videoPlayer.py
@@ -8,19 +8,89 @@
 # WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
+import os
+import time
+from pathlib import Path
+from typing import Tuple
+
+import numpy as np
+import cv2
+
+
 import sys
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
                             QMetaObject, QObject, QPoint, QRect,
                             QSize, QTime, QUrl, Qt,
-                            QStringListModel)
+                            QStringListModel, QThread, Signal, Slot)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
                            QFont, QFontDatabase, QGradient, QIcon,
                            QImage, QKeySequence, QLinearGradient, QPainter,
-                           QPalette, QPixmap, QRadialGradient, QTransform)
+                           QPalette, QPixmap, QRadialGradient, QTransform,
+                           QImage, QPixmap)
 from PySide6.QtWidgets import (QApplication, QDialog, QGraphicsView, QListView,
-                               QPushButton, QSizePolicy, QWidget)
+                               QPushButton, QSizePolicy, QWidget, QLabel, QVBoxLayout)
 
+from PySide6.QtMultimediaWidgets import QVideoWidget
+
+from PySide6.QtMultimedia import (QMediaPlayer)
+
+class VideoThread(QThread):
+    update_frame = Signal(np.ndarray)
+
+    def __init__(self, file_path: str, width: int, height: int, num_frames: int, fps: int):
+        super().__init__()
+
+        self.file_path = file_path
+        self.width = width
+        self.height = height
+        self.num_frames = num_frames
+        self.fps = fps
+
+    def run(self):
+        # open the video file for reading
+        with open(self.file_path, "rb") as file:
+            # read each frame of the video and display it
+            for i in range(self.num_frames):
+                # read the raw pixel data for the current frame
+                raw_data = file.read(self.width * self.height * 3)
+
+                # convert the raw data to a numpy array of pixel values
+                pixels = np.frombuffer(raw_data, dtype=np.uint8).reshape((self.height, self.width, 3))
+
+                # convert the RGB pixels to BGR format for displaying in OpenCV
+                bgr_image = cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR)
+
+                # emit a signal with the current frame data
+                self.update_frame.emit(bgr_image)
+
+                # wait for the specified number of milliseconds before showing the next frame
+                time.sleep(1 / self.fps)
+
+    def stop(self):
+        self.terminate()
+
+
+class VideoWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # create a label widget to display the video frames
+        self.label = QLabel()
+
+        # create a layout for the widget and add the label to it
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+    @Slot(np.ndarray)
+    def update_frame(self, frame: np.ndarray):
+        # convert the frame data to a QImage for display in the label widget
+        qimage = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(qimage)
+
+        # display the frame in the label widget
+        self.label.setPixmap(pixmap)
 
 class Ui_Dialog(object):
 
@@ -39,10 +109,18 @@ class Ui_Dialog(object):
         self.btnStop.setGeometry(QRect(630, 380, 93, 28))
         self.listView = QListView(Dialog)
         self.listView.setObjectName(u"listView")
-        self.listView.setGeometry(QRect(30, 40, 256, 361))
-        self.graphicsView = QGraphicsView(Dialog)
-        self.graphicsView.setObjectName(u"graphicsView")
-        self.graphicsView.setGeometry(QRect(350, 40, 361, 291))
+        self.listView.setGeometry(QRect(30, 40, 256, 361))   
+
+        # create a label widget to display the video frames
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"video_widget")
+        self.label.setGeometry(QRect(350, 40, 361, 291))
+
+        # create a layout for the widget and add the label to it
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        #self.setLayout(layout)
+
 
         self.retranslateUi(Dialog)
 
@@ -60,10 +138,33 @@ class Ui_Dialog(object):
         # Set the model on the list view
         self.listView.setModel(self.model)
 
+        # define the path to the RGB video file and the video parameters
+        file_path = Path("./InputVideo.rgb")
+        width, height = 480, 270
+        fps, num_frames = 30, 8682
+
+        # create a thread to read the video and emit signals with the frame data
+        video_thread = VideoThread(file_path, width, height, num_frames, fps)
+        video_thread.update_frame.connect(self.update_frame)
+
+        # start the thread to read the video
+        video_thread.start()
+
+        # close the OpenCV window
+        cv2.destroyAllWindows()
+
+
+    @Slot(np.ndarray)
+    def update_frame(self, frame: np.ndarray):
+        # convert the frame data to a QImage for display in the label widget
+        qimage = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(qimage)
+
+        # display the frame in the label widget
+        self.label.setPixmap(pixmap)
 
     def play_video(self):
         print("play_video")
-
 
     def pause_video(self):
         print("pause_video")
@@ -84,6 +185,7 @@ class Ui_Dialog(object):
 
 
 if __name__ == '__main__':
+
     # Create the Qt Application
     app = QApplication(sys.argv)
     # Create and show the form
