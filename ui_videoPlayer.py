3044c5e82cb292941d7a98407f75e8d4dac2353d
Lucas <joohan0224@naver.com>
2023-04-24 20:49:47 -0700

    make indices clickable

--- ui_videoPlayer.py
+++ ui_videoPlayer.py
@@ -13,8 +13,13 @@ import time
 from pathlib import Path
 from typing import Tuple
 
+import pyaudio
+import wave
+
 import numpy as np
 import cv2
+
+import re
 
 
 import sys
@@ -29,16 +34,26 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
                            QPalette, QPixmap, QRadialGradient, QTransform,
                            QImage, QPixmap)
 from PySide6.QtWidgets import (QApplication, QDialog, QGraphicsView, QListView,
-                               QPushButton, QSizePolicy, QWidget, QLabel, QVBoxLayout)
+                               QPushButton, QSizePolicy, QWidget, QLabel, QVBoxLayout, QMessageBox)
 
 from PySide6.QtMultimediaWidgets import QVideoWidget
 
 from PySide6.QtMultimedia import (QMediaPlayer)
 
+
+def meanAbsDiff(img1: np.ndarray, img2: np.ndarray) -> float:
+    # Calculate absolute difference
+    abs_diff = cv2.absdiff(img1, img2)
+
+    # Calculate mean of absolute difference
+    mad = np.mean(abs_diff)
+    return mad
+
+
 class VideoThread(QThread):
     update_frame = Signal(np.ndarray)
 
-    def __init__(self, file_path: str, width: int, height: int, num_frames: int, fps: int):
+    def __init__(self, file_path: str, width: int, height: int, num_frames: int, fps: int, startIdx: int):
         super().__init__()
 
         self.file_path = file_path
@@ -49,26 +64,30 @@ class VideoThread(QThread):
         self.threadPaused = False
         self.stop_flag = False
         self.stop_event = False
+        self.start_frame_idx = startIdx
 
     def run(self):
         # open the video file for reading
         with open(self.file_path, "rb") as file:
+
+            file.seek(self.start_frame_idx * self.width * self.height * 3)
+
             # read each frame of the video and display it
-            for i in range(self.num_frames):
+            for i in range(self.start_frame_idx, self.num_frames):
 
                 while self.threadPaused:
                     # print("timeSleep")
                     time.sleep(0.1)
 
                 if self.stop_event:
-                    print("break")
                     break
 
                 # read the raw pixel data for the current frame
                 raw_data = file.read(self.width * self.height * 3)
 
                 # convert the raw data to a numpy array of pixel values
-                pixels = np.frombuffer(raw_data, dtype=np.uint8).reshape((self.height, self.width, 3))
+                pixels = np.frombuffer(raw_data, dtype=np.uint8).reshape(
+                    (self.height, self.width, 3))
 
                 # convert the RGB pixels to BGR format for displaying in OpenCV
                 bgr_image = cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR)
@@ -80,18 +99,82 @@ class VideoThread(QThread):
                 time.sleep(1 / self.fps)
 
     def stop(self):
-        #set the signal to break for loop
+        # set the signal to break for loop
         self.stop_event = True
 
-        #kill the thread and emit the finish signal
+        # kill the thread and emit the finish signal
         self.quit()
         self.finished.emit()
-    
+
+        # refresh the video frame to white
+        whites = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        whites[:, :, :] = 255
+        bgr_image = cv2.cvtColor(whites, cv2.COLOR_RGB2BGR)
+        self.update_frame.emit(bgr_image)
+
     def pause(self):
         self.threadPaused = True
 
-    def resum(self):
+    def resume(self):
         self.threadPaused = False
+
+
+class AudioThread(QThread):
+    def __init__(self, file_path, startIdx: int, chunk_size=1024):
+        super().__init__()
+
+        self.file_path = file_path
+        self.chunk_size = chunk_size
+        self.thread_paused = False
+        self.stop_event = False
+        self.start_frame_idx = startIdx
+        self.fps = 30
+
+    def run(self):
+        wf = wave.open("./InputAudio.wav", 'rb')
+        p = pyaudio.PyAudio()
+
+        print(f'self.start_frame_idx: {self.start_frame_idx}')
+        # set the start position of audio
+        starting_time_offset = self.start_frame_idx / self.fps
+        starting_frame_offset = int(starting_time_offset * wf.getframerate())
+        wf.setpos(starting_frame_offset)
+
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=int(wf.getframerate()),
+                        output=True)
+
+        data = wf.readframes(self.chunk_size)
+        audio_array = np.frombuffer(data, dtype=np.int16)
+
+        while data != b'' and not self.stop_event:
+            while self.thread_paused:
+                time.sleep(0.1)
+
+            if self.stop_event:
+                break
+
+            stream.write(data)
+            data = wf.readframes(self.chunk_size)
+            # time.sleep(0.05)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+    def pause(self):
+        self.thread_paused = True
+
+    def resume(self):
+        self.thread_paused = False
+
+    def stop(self):
+        self.stop_event = True
+
+        # kill the thread and emit the finish signal
+        self.quit()
+        self.finished.emit()
 
 
 class VideoWidget(QWidget):
@@ -109,15 +192,29 @@ class VideoWidget(QWidget):
     @Slot(np.ndarray)
     def update_frame(self, frame: np.ndarray):
         # convert the frame data to a QImage for display in the label widget
-        qimage = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+        qimage = QImage(
+            frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
         pixmap = QPixmap.fromImage(qimage)
 
         # display the frame in the label widget
         self.label.setPixmap(pixmap)
 
+
 class Ui_Dialog(object):
 
-    def __init__(self):
+    def __init__(self, args):
+
+        print("Parameters: ", args)
+
+        # define the path to the RGB video file and the video parameters
+        self.file_path = Path(args[1])
+        self.width, self.height = 480, 270
+        self.fps, self.num_frames = 30, 8682
+
+        self.shot_threshold = 20
+        self.shot_strings = []
+        self.shot_frames = []
+
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
         Dialog.resize(738, 447)
@@ -138,9 +235,9 @@ class Ui_Dialog(object):
 
         self.listView = QListView(Dialog)
         self.listView.setObjectName(u"listView")
-        self.listView.setGeometry(QRect(30, 40, 256, 361))   
-        
-        #video player button attributes
+        self.listView.setGeometry(QRect(30, 40, 256, 361))
+
+        # video player button attributes
         self.start = False
         self.paused = False
 
@@ -153,19 +250,65 @@ class Ui_Dialog(object):
         layout = QVBoxLayout()
         layout.addWidget(self.label)
 
-        #self.setLayout(layout)
+        # self.setLayout(layout)
         self.retranslateUi(Dialog)
 
         QMetaObject.connectSlotsByName(Dialog)
+
+        # audio properties
+        self.start_frame_idx = 0
 
         self.btnPlay.clicked.connect(self.play_video)
         self.btnPause.clicked.connect(self.pause_video)
         self.btnStop.clicked.connect(self.stop_video)
 
-        # Add some strings to the model
-        strings = ["Item 1", "Item 2", "Item 3"]
+        # Connect the clicked signal to the custom slot
+        self.listView.clicked.connect(self.on_item_clicked)
+
+        ###################
+        # Search shots
+        ###################
+        shot_number = 0
+        # open the video file for reading
+        with open(self.file_path, "rb") as file:
+            # read each frame of the video and display it
+            for i in range(self.num_frames):
+
+                # read the raw pixel data for the current frame
+                raw_data = file.read(self.width * self.height * 3)
+
+                # convert the raw data to a numpy array of pixel values
+                pixels = np.frombuffer(raw_data, dtype=np.uint8).reshape(
+                    (self.height, self.width, 3))
+
+                # convert the RGB pixels to BGR format for displaying in OpenCV
+                bgr_image = cv2.cvtColor(pixels, cv2.COLOR_BGR2GRAY)
+
+                # calculate the pixel difference between two frames
+                if i > 0:
+
+                    # Calculate mean of absolute difference
+                    mad = meanAbsDiff(bgr_image, prev_bgr_image)
+
+                    if mad > self.shot_threshold:
+                        shot_number = shot_number + 1
+                        self.shot_strings.append("Shot" + str(shot_number))
+                        self.shot_frames.append(i)
+                        # Print MAD value
+                        print("Mean Absolute Difference: ",
+                              mad, ", #ofFrames: ", i)
+                else:
+                    # For the first frame we do not calculate the pixel difference and just add it to the array.
+                    shot_number = shot_number + 1
+                    self.shot_strings.append("Shot" + str(shot_number))
+                    self.shot_frames.append(i)
+
+                # store the current frame as the previous frame for the next iteration
+                prev_bgr_image = bgr_image.copy()
+
+        # Add strings to the model
         self.model = QStringListModel()
-        self.model.setStringList(strings)
+        self.model.setStringList(self.shot_strings)
 
         # Set the model on the list view
         self.listView.setModel(self.model)
@@ -173,11 +316,11 @@ class Ui_Dialog(object):
         # close the OpenCV window
         cv2.destroyAllWindows()
 
-
     @Slot(np.ndarray)
     def update_frame(self, frame: np.ndarray):
         # convert the frame data to a QImage for display in the label widget
-        qimage = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+        qimage = QImage(
+            frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
         pixmap = QPixmap.fromImage(qimage)
 
         # display the frame in the label widget
@@ -187,43 +330,52 @@ class Ui_Dialog(object):
         print("play_video")
 
         if not self.start:
-            # define the path to the RGB video file and the video parameters
-            file_path = Path("./InputVideo.rgb")
-            width, height = 480, 270
-            fps, num_frames = 30, 8682
 
             # create a thread to read the video and emit signals with the frame data
-            self.video_thread = VideoThread(file_path, width, height, num_frames, fps)
+            self.video_thread = VideoThread(
+                self.file_path, self.width, self.height, self.num_frames, self.fps, self.start_frame_idx)
             self.video_thread.update_frame.connect(self.update_frame)
 
             # start the thread to read the video
             self.video_thread.start()
             self.start = True
 
+            # audio
+            audioFile_path = Path("./InputAudio.wav")
+
+            # create an audio thread
+            self.audio_thread = AudioThread(
+                audioFile_path, self.start_frame_idx)
+            # self.audio_thread.update_frame.connect(self.update_frame)
+            self.audio_thread.start()
+
         if self.start:
             if self.paused:
-                self.video_thread.resum()
+
+                self.audio_thread.resume()
+                self.video_thread.resume()
                 self.paused = False
                 self.btnPlay.setText(
-                QCoreApplication.translate("Dialog", u"Play", None))
+                    QCoreApplication.translate("Dialog", u"Play", None))
 
     def pause_video(self):
-        #set paused to True 
+        # set paused to True
         self.video_thread.pause()
+        self.audio_thread.pause()
         if not self.paused:
             self.paused = True
             self.btnPlay.setText(
-            QCoreApplication.translate("Dialog", u"Resum", None))
-
+                QCoreApplication.translate("Dialog", u"Resume", None))
 
     def stop_video(self):
         # call stop function in the thread class
         self.video_thread.stop()
-
+        self.audio_thread.stop()
+        del self.video_thread
+        del self.audio_thread
         # reset the all related var
         self.start = False
         self.paused = False
-
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(
@@ -236,16 +388,50 @@ class Ui_Dialog(object):
             QCoreApplication.translate("Dialog", u"Stop", None))
     # retranslateUi
 
+    # Define a custom slot to handle the click event
+    @Slot('QModelIndex')
+    def on_item_clicked(self, index):
+        item = index.data(Qt.DisplayRole)  # e.g. 'shot14'
+
+        match = re.search(r'\d+$', item)
+
+        if match:
+            # regex takes a time, so if this causes issues, use anothery way.
+            shot_idx = int(match.group())
+
+        # Move both the video and audio frame into the selected one by the user.
+        frame_idx = self.shot_frames[shot_idx-1]
+
+        # kill both video and audio threads, and start from the given frame index.
+        self.move_to_frame(frame_idx)
+
+        # Below is just for debugging.
+        # QMessageBox.information(
+        #     self.listView, 'Item Clicked', f'You clicked on {item}')
+
+    def move_to_frame(self, frame_idx: int):
+
+        # stop and kill all threads
+        if(self.start):
+            self.stop_video()
+
+        # pass frame_idx into VideoThread and AudioThread to start Video and Audio
+        self.start_frame_idx = frame_idx
+        print(f'frame_idx1: {frame_idx}')
+        self.play_video()
+        print(f'frame_idx2: {frame_idx}')
+
 
 if __name__ == '__main__':
 
     # Create the Qt Application
     app = QApplication(sys.argv)
+
     # Create and show the form
     Dialog = QDialog()
-    ui = Ui_Dialog()
-    #ui.setupUi(Dialog)
+    ui = Ui_Dialog(sys.argv)
     Dialog.show()
+
     # Start the event loop
     app.exec()
 
